76626b5cffe73d48396a39cfbae746765c3046b7
Yann Klis <yann.klis@gmail.com>
2018-02-19 12:26:46 +0100

    ripple for mdc-fab button as well (only if they are actual links)

--- js/ripple.js
+++ js/ripple.js
@@ -1,7 +1,7 @@
 import {MDCRipple} from '@material/ripple'
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.mdc-button').forEach((elem) => {
+  document.querySelectorAll('.mdc-button, a.mdc-fab').forEach((elem) => {
     new MDCRipple(elem)
   })
 })
